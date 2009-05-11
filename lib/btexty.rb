fa4aef461fd6be200e3b99aaace986cc31365538
Mark Turner <mark@amerine.net>
2009-05-11 16:52:00 -0700

    tweaking api call for unread messages

--- lib/btexty.rb
+++ lib/btexty.rb
@@ -5,23 +5,35 @@ require File.dirname(__FILE__) + '/btexty/data'
 
 class BTexty
   include HTTParty
-  base_uri "http://www.btexty.com/api"
-  attr_accessor :phone_number, :pin
+  format :json
   
-  def initialize(phone_number, pin)
-    phone_number.gsub!(/\W/,'')
-    phone_number.gsub!(/\A/, '1') if phone_number.length == 10
-    phone_number.gsub!(/\A/,'+')
-    @phone_number, @pin = phone_number, pin
+  attr_accessor :phone_number, :pin, :method
+  
+  def initialize(phone_number, pin, method='json')
+    clean_phone_number(phone_number)
+    @phone_number, @pin, @method = phone_number, pin, method
   end
   
   def get(path, options={})
     options.merge!({:basic_auth => {:username => @phone_number, :password => @pin}, 
-      :method => 'xml'})
-    self.class.get("/#{options[:method]}/#{path}")
+      :method => @method})
+    self.class.get("http://www.btexty.com/api/#{options[:method]}/#{path}", options)
   end
   
   def unread
-    get('inbox/unread')
+    get('inbox/unread')['response']['body']['unread']
+  end
+  
+  def messages
+    get('inbox/index')
+  end
+  
+  private
+  
+  def clean_phone_number(phone_number)
+    phone_number.gsub!(/\W/,'')
+    phone_number.gsub!(/\A/, '1') if phone_number.length == 10
+    phone_number.gsub!(/\A/,'+')
+    phone_number
   end
 end
