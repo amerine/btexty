eed4d8c76e40eb3497abdff17997c204f44fa4d0
Mark Turner <mark@amerine.net>
2009-05-12 12:06:17 -0700

    Added Message class and parsing for messages

--- lib/btexty.rb
+++ lib/btexty.rb
@@ -5,11 +5,11 @@ require File.dirname(__FILE__) + '/btexty/data'
 
 class BTexty
   include HTTParty
-  format :xml
+  format :json
   
   attr_accessor :phone_number, :pin, :method
   
-  def initialize(phone_number, pin, method='xml')
+  def initialize(phone_number, pin, method='json')
     clean_phone_number(phone_number)
     @phone_number, @pin, @method = phone_number, pin, method
   end
@@ -25,7 +25,23 @@ class BTexty
   end
   
   def messages
-    messages = get('inbox/index')['response']['body']
+    return_messages = []
+    messages = get('inbox/index')['response']['body']['messages']
+    messages.each do |message|
+      process_message = Message.new
+      process_message.content = message['message']['content']
+      process_message.thread = message['message']['thread']
+      process_message.sender = message['message']['sender']
+      process_message.recipients = message['message']['recipients']
+      process_message.sender_name = message['message']['sender_name']
+      process_message.recipients_names = message['message']['recipients_names']
+      process_message.is_read = message['message']['is_read']
+      process_message.wall = message['message']['wall']
+      process_message.created_on = message['message']['created_on']
+      process_message.id = message['message']['id']
+      return_messages << process_message
+    end
+    return_messages
   end
   
   private
---
+++ lib/btexty/data.rb
@@ -0,0 +1,8 @@
+class BTexty
+  
+  class Message
+    attr_accessor :content, :thread, :sender, :recipients, :sender_name
+    attr_accessor :recipients_names, :is_read, :wall, :created_on, :id
+  end
+  
+end
