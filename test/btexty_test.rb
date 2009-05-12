e4eed105d74edc0a939bd3a20eaddc3a6e481251
Mark Turner <mark@amerine.net>
2009-05-12 14:00:16 -0700

    Committing the tests, its been a while

--- test/btexty_test.rb
+++ test/btexty_test.rb
@@ -20,5 +20,40 @@ class BTextyTest < Test::Unit::TestCase
     end
   end
   
+  context "Checking for unread messages" do
+    setup do
+      FakeWeb.register_uri(:get, "http://+15419777188:12341@www.btexty.com:80/api/json/inbox/unread", :string => File.read('test/fixtures/unread.json'))
+      @btexty = BTexty.new('+15419777188', 12341)
+    end
+    
+    should "return the number of unread messages as an interger" do
+      assert_equal 1, @btexty.unread
+    end
+  end
+  
+  context "Getting the list a list messages" do
+    setup do 
+      FakeWeb.register_uri(:get, "http://+15419777188:12341@www.btexty.com:80/api/json/inbox/index", :string => File.read('test/fixtures/messages.json'))
+      @btexty = BTexty.new('+15419777188', 12341)
+    end
+    
+    should "return a list of messages" do
+      assert_equal 1, @btexty.messages.size
+    end
+    
+    should "allow you to read each messages attributes" do
+      message = @btexty.messages.first
+      message.content.should == 'Welcome to btexty! Click on the link we just sent you as text message to activate your account.'
+      message.thread.should == '9xknm7nn3icqa3qnqxni0uwaooz6k6uiupbuij7tao38b4ax1o2b57dsx0onphx5'
+      message.sender.should == '+18579989143'
+      message.recipients.should == '+15419777188'
+      message.sender_name.should == 'btexty'
+      message.recipients_names.should == 'me'
+      message.is_read.should == false
+      message.wall.should == false
+      message.created_on.should == '2009-05-11 22:32:43.643042'
+      message.id.should == 'agZtb3dhbGxyIQsSB0FjY291bnQYAgwLEgxTaG9ydE1lc3NhZ2UY_50CDA'
+    end
+  end
   
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,9 @@ require 'rubygems'
 require 'test/unit'
 require 'shoulda'
 require 'matchy'
+require 'fakeweb'
+
+FakeWeb.allow_net_connect = false
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
